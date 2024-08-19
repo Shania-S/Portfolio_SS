0f3593821a391b144bb4e0a368656b3a34981f4b
Shania Sidimana <shaniasidimana1@gmail.com>
2024-08-19 09:31:53 +0200

    Add text compressor and file minifier

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,20 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import viteCompression from 'vite-plugin-compression';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteCompression({
+      algorithm: 'brotliCompress',
+      ext: '.br',
+      threshold: 10240,
+      deleteOriginFile: false,
+    }),
+  ],
   base: '/Portfolio_SS/',
+  build: {
+    minify: 'esbuild',
+  },
 });
